db23bf246e00e5d2f089a1b8e35f328d72e3d753
leonardo-at <leoat@live.com>
2021-06-12 15:59:54 -0300

    adicionando exercícios das aulas dos dias 14 e 16 de junho. Arrow Functions e Objetos Literais

---
+++ ProgramacaoImperativaExs/T3LeoArrowFun1406.js
@@ -0,0 +1,9 @@
+//Ex1
+let mensagem = () => "Olá, bom dia!"
+console.log(mensagem())
+
+console.log("-------------------------")
+
+//Ex2
+let somar = (a,b) => a + b;
+console.log(somar(10,10))
---
+++ ProgramacaoImperativaExs/T3LeoObjLit1606.js
@@ -0,0 +1,16 @@
+//Ex1
+let numerosPrimos = [2, 3, 5, 7, 11]
+
+let numerosExercicio = [1, 4, 6, 8, 9, 10]
+
+let todosOsNumeros = [...numerosPrimos, ...numerosExercicio]
+
+console.log(todosOsNumeros)
+
+console.log("-----------------------")
+
+//Ex2
+let maiorNumero = [3, 5, 8, 15, 20, 34, 49, 123, 0.1, 0.2];
+console.log(Math.min(...maiorNumero));
+
+console.log("Não entendi direito o exercício 2, é isso?")
